3034c092d2a0af9998a672f610dd523be048779a
anirudh <anirudh07.anand@gmail.com>
2022-06-10 14:32:24 +0530

    latest version

--- combined_code/comparison_results/comp.py
+++ combined_code/comparison_results/comp.py
@@ -14,13 +14,14 @@ df2 = pd.read_csv("output.csv",names=["v_id","f_id","vehicle","empty","time"])
 df2["marked"] = "not found"
 df1["marked"] = "-1"
 for index1, row1 in df1.iterrows():
-#   print(index1)
+  print(index1, row1)
   for index2, row2 in df2.iterrows():
-      if(row2["marked"] !="found" and row1["vehicle"] == row2["vehicle"] and row2["time"]>row1["time"]):
-          row1["marked"] = str(int(row2["time"])-int(row1["time"]))
-          row2["marked"] ="found"
-        #   print(row1)
-        #   print(row2)
-          break
+    print(index2, row2)
+    if(row2["marked"] !="found" and row1["vehicle"] == row2["vehicle"] and row2["time"]>row1["time"]):
+        row1["marked"] = str(int(row2["time"])-int(row1["time"]))
+        row2["marked"] ="found"
+        print(row1)
+        print(row2)
+        break
 df1.to_csv('video1.csv')
 df2.to_csv('video2.csv')
