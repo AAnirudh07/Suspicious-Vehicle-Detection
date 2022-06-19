ae041019a12d5c59d5c8ad53de28378308654385
anirudh <anirudh07.anand@gmail.com>
2022-06-19 09:30:44 +0530

    add roi param

--- candidate_localizers/object_tracking_roi/object_tracker/main.py
+++ candidate_localizers/object_tracking_roi/object_tracker/main.py
@@ -3,14 +3,14 @@ from tracker import *
 
 tracker = EuclideanDistTracker()
 
-cap = cv2.VideoCapture("D:\Desktop\\test_pics\\in.mp4")
+cap = cv2.VideoCapture("D:\\Projects\\vehicle classification\\data\\videos\\Ch8_20220112161012.mp4")
 object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
 while True:
     ret, frame = cap.read()
     height, width, _ = frame.shape
 
-    roi = frame[550: 1080,:]
+    roi = frame[400: 1080,:]
 
     mask = object_detector.apply(roi)
     _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
